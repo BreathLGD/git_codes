efbaca73574b0ca7aff53cf6de294655be297ef6
Priyanshu <priyanshu199917@gmail.com>
2022-06-24 21:15:38 +0530

    repo

---
+++ auto.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+wget https://addons.mozilla.org/firefox/downloads/file/3961087/ublock_origin-1.43.0.xpi
+
+wget https://addons.mozilla.org/firefox/downloads/file/3960137/bitwarden_password_manager-2022.5.0.xpi
+
+
+sudo apt update 
+sudo apt install fireofox
+
